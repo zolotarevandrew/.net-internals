04cc245f0ead34619ed5cd8d2429009a3e8dd026
Andrew Zolotarev <gagasusrock@gmail.com>
2022-01-20 19:58:59 +0300

    MutexInternals

---
+++ Concurrent/MutexInternals.cs
@@ -0,0 +1,37 @@
+﻿namespace Concurrent;
+
+public static class MutexInternals
+{
+    private static Mutex mutex = new Mutex();  
+    private const int numhits = 1;  
+    private const int numThreads = 4;  
+    
+    private static async Task TaskProcess()  
+    {  
+        for (int i = 0; i < numhits; i++)  
+        {  
+            mutex.WaitOne();  
+            Console.WriteLine("{0} has entered in the C_sharpcorner.com", Thread.CurrentThread.ManagedThreadId);  
+          
+            await Task.Delay(500).ConfigureAwait(false);  
+            
+            Console.WriteLine("{0} is leaving the C_sharpcorner.com\r\n", Thread.CurrentThread.ManagedThreadId);  
+            mutex.ReleaseMutex();   
+        }
+    }
+
+    public static async Task ExecuteSimpleAsync()
+    {
+        var tasks = new List<Task>();
+        for (int i = 0; i < numThreads; i++)
+        {
+            var task = Task.Run(TaskProcess);
+            tasks.Add(task);
+        }
+
+        await Task.WhenAll(tasks);
+        
+        //will never be executed
+        Console.WriteLine("Done");  
+    }  
+}
---
+++ Concurrent/TimedLock.cs
@@ -0,0 +1,46 @@
+﻿namespace Concurrent;
+
+public class TimedLock
+{
+    private readonly object _toLock;
+
+    public TimedLock(object toLock)
+    {
+        this._toLock = toLock;
+    }
+
+    public LockReleaser Lock(TimeSpan timeout)
+    {
+        if (Monitor.TryEnter(_toLock, timeout))
+        {
+            return new LockReleaser(_toLock);
+        }
+        throw new TimeoutException();
+    }
+
+    public struct LockReleaser : IDisposable
+    {
+        private readonly object _toRelease;
+
+        public LockReleaser(object toRelease)
+        {
+            this._toRelease = toRelease;
+        }
+        
+        public void Dispose()
+        {
+            Monitor.Exit(_toRelease);
+        }
+    }
+
+    public static void Process()
+    {
+        object guard = new object();
+
+        using(new TimedLock(guard).Lock(TimeSpan.FromSeconds(2)))
+        {
+            
+        }
+    }
+}
+
